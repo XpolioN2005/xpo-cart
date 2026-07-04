10c9b4645083faea278a560c4db89ffcebaa7f67
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-04 22:55:18 +0530

    setup - basic networking

--- Network/network_manager.gd
+++ Network/network_manager.gd
@@ -10,8 +10,19 @@ func create_server():
 	print("Server created!")
 
 func create_client(host_ip: String = "localhost", host_port: int = SERVER_PORT):
+	_setup_client_connection_signals()
+
 	var enet_network_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	enet_network_peer.create_client(host_ip, host_port)
 	get_tree().get_multiplayer().multiplayer_peer = enet_network_peer
-	
+
 	print("Client peer created!")
+
+
+func _setup_client_connection_signals():
+	if not get_tree().get_multiplayer().server_disconnected.is_connected(_server_disconnected):
+		get_tree().get_multiplayer().server_disconnected.connect(_server_disconnected)
+
+func _server_disconnected():
+	print("Server disconnected")
+	get_tree().get_multiplayer().multiplayer_peer = null
